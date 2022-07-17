628af190ae486adb6b9f17b692c88de48085e0bf
yoshi1075 <agy6786@gmail.com>
2022-07-17 17:45:56 +0900

    first commit

---
+++ SwiftUI_Basic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Basic
+//
+//  Created by Kato Yoshihiro on 2022/07/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Basic/SwiftUI_BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicApp.swift
+//  SwiftUI_Basic
+//
+//  Created by Kato Yoshihiro on 2022/07/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
